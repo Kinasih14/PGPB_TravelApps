e91bf96a7b5d5ec0102281620e697db632c4dc4f
Kinasih14 <142877930+Kinasih14@users.noreply.github.com>
2023-11-07 07:25:03 +0700

    Add project
    Add project

---
+++ app/src/main/java/com/example/travelappkikin/HomeFragmentDirections.java
@@ -0,0 +1,11 @@
+package com.example.travelappkikin;
+
+import androidx.navigation.NavDirections;
+
+public class HomeFragmentDirections {
+    private static NavDirections args;
+
+    public static NavDirections actionHomeFragmentToCheckoutFragment(String toString) {
+        return args;
+    }
+}
---
+++ app/src/main/java/com/example/travelappkikin/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.travelappkikin;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import android.os.Bundle;
+import com.example.travelappkikin.databinding.ActivityMainBinding;
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        NavController navController =
+                NavHostFragment.findNavController(getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment));
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,
+                navController);
+
+
+        setContentView(binding.getRoot());
+    }
+}
